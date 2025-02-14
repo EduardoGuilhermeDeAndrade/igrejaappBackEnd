4f8568217347d4f5e402211a0a41bf1f81740dda
EduardoG <edu.guiandrade@gmail.com>
2025-02-14 19:51:31 -0300

    Altera entidade igreja dto e coloca campo complemento

--- igreja.Application/DTOs/Church/ChurchDto.cs
+++ igreja.Application/DTOs/Church/ChurchDto.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        public string Complement { get; set; }
 
     }
 }
