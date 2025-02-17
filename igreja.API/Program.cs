e4ebd57dfefe3c4f2457e5ce67195ad98ed89a7a
EduardoG <edu.guiandrade@gmail.com>
2025-02-17 14:55:58 -0300

    Inicia com o seeddata normalmente.

--- igreja.API/Program.cs
+++ igreja.API/Program.cs
@@ -113,7 +113,8 @@ using (var scope = app.Services.CreateScope())
     context.Database.Migrate();
 
     // Inicializa os dados
-    //SeedData.Initialize(context);
+
+   SeedData.Initialize(context);
 }
 
 if (app.Environment.IsDevelopment())
